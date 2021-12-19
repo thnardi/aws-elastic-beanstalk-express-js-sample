d915d4501c81695dae4199eb3d8e467e533e4afa
Thiago Nardi <thnardi@gmail.com>
2021-12-19 11:45:23 -0300

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Dele que dele, trope and galope.'));
+app.get('/', (req, res) => res.send('Dele que dele, trope and galope. Esse código precisa ser aprovado manualmente.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
